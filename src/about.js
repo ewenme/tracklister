9c0aa527b635d22cd60784ed539ff10513062067
ewen_ <ewenhenderson@gmail.com>
2021-10-29 21:44:29 +0100

    style about page

--- src/about.js
+++ src/about.js
@@ -14,12 +14,10 @@ function About() {
       >
         {close => (
           <div className="modal">
-            <a href="/" className="close" onClick={close}>
-              &times;
-            </a>
-            <div className="header"> about / help </div>
-            <div className="content">
+            <div className="prose lg:prose-xl">
+            <h2> about / help </h2>
               {" "}
+              <p>
               A simple app to turn .txt files exported from{" "}
               <a href="https://rekordbox.com/en/">Rekordbox</a> and .nml files
               exported from{" "}
@@ -28,13 +26,17 @@ function About() {
               <a href="https://reactjs.org/">React</a>. Made by{" "}
               <a href="https://twitter.com/ewen_">@ewen_</a>, source code on{" "}
               <a href="https://github.com/ewenme/tracklister">Github</a>.
-              <h5>Rekordbox support</h5>
+              </p>
+              <h4>Rekordbox support</h4>
+              <p>
               Any playlist can be exported from Rekordbox by right-clicking,
               selecting "Export a playlist to a file", then "Export a playlist
               to a file for KUVO (*.txt)". Note that history files can be
               exported in the same way - find them under the "Histories" tab
               inside the explorer pane.
-              <h5>Traktor support</h5>
+              </p>
+              <h4>Traktor support</h4>
+              <p>
               Any playlist can be exported from Traktor by right-clicking, then
               selecting "Export Playlist", and ensuring "NML" filetype is
               active. Traktor also exports all sessions as .nml history files -
@@ -43,10 +45,11 @@ function About() {
                 this article
               </a>{" "}
               for more info.
+              </p>
             </div>
-            <div className="actions">
+            <div className="flex flex-row justify-end">
               <button
-                className="muted-button"
+                className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
                 onClick={() => {
                   close();
                 }}
