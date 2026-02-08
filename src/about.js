ff3ae4d2c88c50f7b5352616a74c24c33d65f67f
ewenme <ewenyours@proton.me>
2026-02-08 19:00:29 +0000

    remove feedback sections

--- src/about.js
+++ src/about.js
@@ -11,31 +11,21 @@ function About() {
               <a href="https://en.wikipedia.org/wiki/Traktor">Traktor</a> into
               tracklists.
               </p>
-              <p>
-              <a href="https://ewen6.typeform.com/to/meXt691B">Feedback welcome :]</a>
-              </p>
-              <p>
-              <a href="https://github.com/ewenme/tracklister">Pull Requests also welcome !</a>
-              </p>
               <h4>Rekordbox support</h4>
               <p>
-              Any playlist can be exported from Rekordbox by right-clicking,
-              selecting "Export a playlist to a file", then "Export a playlist
-              to a file for KUVO (*.txt)". 
-              N.B. history files can be
-              exported in the same way — find them under the "Histories" tab
-              inside the explorer pane.
+              To export a playlist or history file from Rekordbox, right-click it and
+              select "Export a playlist to a file" → "Export a playlist to a
+              file for KUVO (*.txt)".
               </p>
               <h4>Traktor support</h4>
               <p>
-              Any playlist can be exported from Traktor by right-clicking, then
-              selecting "Export Playlist", and ensuring "NML" filetype is
-              active. Traktor also exports all sessions as .nml history files —
-              see{" "}
+              To export a playlist from Traktor, right-click it, select
+              "Export Playlist", and ensure the filetype is set to "NML".
+              Traktor saves all sessions as .nml history files —{" "}
               <a href="https://support.native-instruments.com/hc/en-us/articles/210274225-What-Files-and-Folders-does-TRAKTOR-Install-on-my-System-">
-                this article
-              </a>{" "}
-              for more info.
+                learn more here
+              </a>
+              .
               </p>
             </div>
     )
